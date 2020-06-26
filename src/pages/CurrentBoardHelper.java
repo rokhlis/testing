3f24818da14806a35c6922ba551a84e0c443d074
rokhlis <rokhlis@gmail.com>
2020-06-26 23:14:33 +0300

    commented

--- src/pages/CurrentBoardHelper.java
+++ src/pages/CurrentBoardHelper.java
@@ -25,8 +25,36 @@ public class CurrentBoardHelper extends PageBase{
         waitUntilElementIsVisible((By.xpath(boardLocator(BOARD_TITLE))), 20);
     }
 
+    public void createNewListWithNameSpecified(String listName){
+        driver.findElement(By.xpath("//div[@class='js-add-list list-wrapper mod-add is-idle']//span[@class='placeholder']")).click();
+        waitUntilElementIsVisible((By.xpath("//input[@class='primary mod-list-add-button js-save-edit']")), 20);
+        //Entering new List name
+        driver.findElement(By.name("name")).sendKeys(listName);
+        waitUntilElementIsClickable(By.xpath("//input[@class='primary mod-list-add-button js-save-edit']"), 20);
+        //Adding new List with entered name
+        driver.findElement(By.xpath("//input[@class='primary mod-list-add-button js-save-edit']")).click();
+        //Canceling other List adding
+        waitUntilElementIsVisible((By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel-edit']")), 20);
+        driver.findElement(By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel-edit']")).click();
+    }
+
+    public void createNewCardWithNameSpecified(String cardName){
+        //Opening card composer
+        driver.findElement(By.className("js-open-card-composer")).click();
+        waitUntilElementIsVisible((By.className("js-add-card")), 20);
+        //Entering card name
+        driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']")).sendKeys(cardName);
+        waitUntilElementIsClickable((By.className("primary confirm mod-compact js-add-card")), 0);
+        //Adding card to the list
+        driver.findElement(By.xpath("//input[@class='primary confirm mod-compact js-add-card']")).click();
+    }
+
     public void checkQuantityOfLists(){
         System.out.println("Quantity of Lists: " + driver.findElements(By.className("js-list")).size());
+    }
+
+    public void checkQuantityOfCards(){
+        System.out.println("Quantity of Cards: " + driver.findElements(By.className("js-card-name")).size());
     }
 
     public String boardLocator(String boardTitle) {
